02f85988c559bfa0fca815d506f0e071bb6160ed
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-05 19:15:07 +0700

    bugs fixed, workable task

--- Seminar1_Task2/Program.cs
+++ Seminar1_Task2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число для сравнения : ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число для сравнения : ");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (FirstNumber > SecondNumber)
+    {
+        Console.WriteLine("При сравнении 2-х чисел: " + FirstNumber +" и " + SecondNumber + ", большим является: " +FirstNumber);
+    }
+else if (FirstNumber < SecondNumber)
+    {
+        Console.WriteLine("При сравнении 2-х чисел: " + FirstNumber +" и " + SecondNumber + ", большим является: " +SecondNumber);
+    }
+else 
+    {
+        Console.WriteLine("При сравнении 2-х чисел: " + FirstNumber +" и " + SecondNumber + ", выявлено их равенство.");
+    }
+

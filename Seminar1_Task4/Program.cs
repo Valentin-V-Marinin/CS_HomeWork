72fc028991cb5e61f9c03369adc0c50e2e0d82cd
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-07 06:41:55 +0700

    HomeWork Seminar1 Task4

--- Seminar1_Task4/Program.cs
+++ Seminar1_Task4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Домашнее задание
+// Семинар 1, Задача 4
+// Поиск максимального числа из 3-х введенных чисел
+
+int max = 0;
+int inputNumber = 0;
+int count = 1;
+
+while (count < 4)
+    {
+        Console.WriteLine("введите число №" + count);
+        
+        if (count == 1)
+        {
+            max = Convert.ToInt32(Console.ReadLine());
+        }
+        else
+        {
+            inputNumber = Convert.ToInt32(Console.ReadLine());
+            if (inputNumber > max) max = inputNumber;
+        }
+                
+        count++;
+    }
+ 
+Console.WriteLine("максимальное число: " + max);

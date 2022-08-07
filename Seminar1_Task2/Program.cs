436bac261f30bae7c5345c54987c5dad9ce30491
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-07 07:11:20 +0700

    HomeWork Seminar1 Task2 add description

--- Seminar1_Task2/Program.cs
+++ Seminar1_Task2/Program.cs
@@ -1,15 +1,23 @@
-﻿Console.WriteLine("Введите первое число для сравнения : ");
+﻿// Домашнее задание
+// Семинар 1, Задача 2
+// Определение большего и меньшего из 2-х чисел
+
+Console.WriteLine("Введите первое число для сравнения : ");
 int FirstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число для сравнения : ");
 int SecondNumber = Convert.ToInt32(Console.ReadLine());
 
 if (FirstNumber > SecondNumber)
     {
-        Console.WriteLine("При сравнении 2-х чисел: " + FirstNumber +" и " + SecondNumber + ", большим является: " +FirstNumber);
+        Console.WriteLine("При сравнении 2-х чисел: " + FirstNumber +" и " + SecondNumber +
+                          ", большим является: " + FirstNumber +
+                          ", меньшим является: " + SecondNumber);
     }
 else if (FirstNumber < SecondNumber)
     {
-        Console.WriteLine("При сравнении 2-х чисел: " + FirstNumber +" и " + SecondNumber + ", большим является: " +SecondNumber);
+        Console.WriteLine("При сравнении 2-х чисел: " + FirstNumber +" и " + SecondNumber + 
+                          ", большим является: " + SecondNumber +
+                          ", меньшим является: " + FirstNumber);
     }
 else 
     {

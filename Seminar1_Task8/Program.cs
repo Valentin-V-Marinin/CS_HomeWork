dd0fff9aabf9dbce27aca430a3ab38b2e8389722
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-07 08:44:31 +0700

    HomeWork Seminar1 Task8

---
+++ Seminar1_Task8/Program.cs
@@ -0,0 +1,25 @@
+﻿// Домашнее задание
+// Семинар 1, Задача 8
+// Вывод списка четных чисел между 1 и введенным числом (number)
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if (number > 0)
+{
+    while (count <= number)
+    {
+        if (count % 2 == 0) Console.WriteLine(count);
+        count++;
+    }
+}
+else
+{
+    while (count >= number)
+    {
+        if (count % 2 == 0) Console.WriteLine(count);
+        count--;
+    }
+};
+

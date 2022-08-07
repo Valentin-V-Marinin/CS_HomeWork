80bedf3bc0d21fa5f0c9a8d4b3158c71403cb5b0
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-07 07:53:13 +0700

    HomeWork Seminar1 Task6

---
+++ Seminar1_Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Домашнее задание
+// Семинар 1, Задача 6
+// Определение четности введенного числа
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32( Console.ReadLine() );
+
+if ((number % 2) == 0) 
+    {
+        Console.WriteLine("Введеннле число является четным");
+    }
+else
+    {
+        Console.WriteLine("Введеннле число не является четным");
+    };
+

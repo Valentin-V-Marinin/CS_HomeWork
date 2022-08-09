e6565d4e3123d74334b5ad80b016039d2fdbaf10
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-09 22:32:54 +0700

    HomeWork Seminar2 Task13

---
+++ Seminar2_Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Домашнее задание
+// Семинар 2, Задача 13
+// Вывод третьей цифры заданного числа или вывод сщщбщения о её отсутствии
+
+Console.WriteLine("Введите число");
+string? number = Console.ReadLine();
+
+if (Convert.ToInt32(number) > 0)
+{
+    if (number.Length < 3)
+        {Console.WriteLine($"В числе {number} нет третьей цифры");}
+    else
+        {Console.WriteLine($"В числе {number} третьей цифрой является {number[2]}");}
+}
+else
+{
+    if (number.Length < 4)
+        {Console.WriteLine($"В числе {number} нет третьей цифры");}
+    else
+        {Console.WriteLine($"В числе {number} третьей цифрой является {number[3]}");}
+}

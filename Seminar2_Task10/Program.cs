baf92416ca8b99ba232635be5d3fdab07d93300e
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-09 21:12:41 +0700

    HomeWork Seminar2  Task10

---
+++ Seminar2_Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Домашнее задание
+// Семинар 2, Задача 10
+// Ввод трехзначного числа, вывод его второй цифры
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number > 99 & number < 1000) || (number > -1000 & number < -99))
+{
+    Console.WriteLine($"Вторая цифра числа {number} - {Math.Abs((number/10)%10)}");
+}
+else
+{
+    Console.WriteLine($"Введенное число {number} не является трехзначным");
+}

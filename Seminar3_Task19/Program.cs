be2500fba1e38850f89e1a4c0dd82b3aa05fe950
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-13 00:35:52 +0700

    HomeWork Seminar3 Task19

---
+++ Seminar3_Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Домашнее задание
+// Семинар 3, Задача 19
+// Ввод 5-тизначного числа и определение наличия палиндрома  
+
+Console.WriteLine("Введите 5-тизначное число:");
+int Number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if (Number >9999 && Number <100000)
+{
+    string NumberS = Convert.ToString(Number);
+    if (NumberS[0] == NumberS[4] && NumberS[1] == NumberS[3])
+    {
+        Console.WriteLine($" {Number} - это число-палиндром.");
+    }
+    else
+    {
+        Console.WriteLine($" {Number} - это число НЕ палиндром.");
+    }
+}
+else
+{
+    Console.WriteLine($"Введённое число {Number} не пятизначное!");
+}

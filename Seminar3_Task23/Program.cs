cdb1834c21e05a76498fca9f26f7dbff07f1647d
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-13 11:22:24 +0700

    HomeWork Seminar3 Task23

---
+++ Seminar3_Task23/Program.cs
@@ -0,0 +1,36 @@
+﻿// Домашнее задание
+// Семинар 3, Задача 23
+// Вывод кубов чисел от 1 до введенного N
+
+Console.WriteLine("Введите число N");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+string MyCubes(int CountTo)
+{
+    int count = 1;
+    string result = "";
+    if (N > 0)
+    {
+        while (count <= CountTo)
+        {
+            result = result + Math.Pow(count,3) + ", ";
+            count++;
+        }
+    }
+    else
+    {
+        while (CountTo <= count)
+        {
+            result = result + Math.Pow(CountTo,3) + ", ";
+            CountTo++;
+        }
+    }
+
+    return result.TrimEnd(new char[] {',', ' '});
+}
+
+Console.WriteLine(MyCubes(N));
+
+
+

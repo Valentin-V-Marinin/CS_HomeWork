d24beae1e3f29f29157fe40cb97c884efa7fa21c
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-16 23:37:05 +0700

    HomeWork Seminar4 Task29

---
+++ Seminar4_Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Домашнее задание
+// Семинар 4, Задача 29
+// Приём на вход массива из 8 элементов, вывод массива 
+
+
+int[] array = new int[8];
+
+// заполняем массив
+for (int i = 0; i < 8; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент массива:");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Полученный массив: [" + String.Join(" ,", array) + "]");
+
+

69c6add318c2f63d9018873b8317fb055f63a165
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-16 15:12:14 +0700

    HomeWork Seminar4 Task25

---
+++ Seminar4_Task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Домашнее задание
+// Семинар 4, Задача 25
+// Приём на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+string[] Arr = {"A", "B"};
+int[] ArrNumb = new int [2];
+int count = 0;
+
+while (count < 2)
+{
+    Console.WriteLine($"Введите число {Arr[count]}: ");
+    ArrNumb[count] = Convert.ToInt32(Console.ReadLine());
+    count++;
+}
+
+int result = 1; count = 0;
+ 
+while (count < ArrNumb[1])
+{
+    result = result * ArrNumb[0];
+    count++;
+}
+
+Console.WriteLine(result);

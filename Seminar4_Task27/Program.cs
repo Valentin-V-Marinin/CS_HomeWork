ca768855f0dd5a7e562377176e24f13e85b960a8
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-16 17:24:08 +0700

    HomeWork Seminar4 Task27

---
+++ Seminar4_Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Домашнее задание
+// Семинар 4, Задача 25
+// Приём на вход числа, вывод суммы его цифр 
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string numbStr = Convert.ToString(Math.Abs(number));
+int count = 0; 
+int sum = 0;
+
+// если введено отрицательное число, считаем первую цифру числа отрицательной
+while (count < numbStr.Length)
+{
+    if (number < 0 & count ==0)
+        {sum += Convert.ToInt32("" + numbStr[count]) * -1;}
+    else   
+        {sum += Convert.ToInt32("" + numbStr[count]);}
+    count++;
+}
+
+Console.WriteLine($"Сумма цифр числа равна: {sum}");

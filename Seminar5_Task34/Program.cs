53d57165a8ae68eef99d8e065ceba9a916a5a083
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-19 16:47:47 +0700

    HomeWork Seminar5 Task34

---
+++ Seminar5_Task34/Program.cs
@@ -0,0 +1,20 @@
+﻿// Домашнее задание
+// Семинар 5, Задача 34
+// Генерация массива с трехзначными положительными числами,
+//   вывод количества четных чисел в массиве
+
+Console.WriteLine("Введите количество элементов в массиве: ");
+int arrLength  = Convert.ToInt32(Console.ReadLine()) ;
+
+int evenNumber = 0;
+int[] arr = new int [arrLength];
+Random Rand = new Random();
+
+for(int i = 0; i < arrLength; i++)
+{
+    arr[i] = Rand.Next(100,1000);
+    if (arr[i]%2 == 0) evenNumber+= 1;
+}
+
+Console.WriteLine("[" + string.Join(", ", arr) +"]");
+Console.WriteLine($"Количество четных чисел в массиве: {evenNumber} ");

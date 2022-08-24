30277f80e1c75fe8eb67952a3142aa81ad47ad3a
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-24 17:54:54 +0700

    HomeWork Seminar6 Task44

---
+++ Seminar6_Task44/Program.cs
@@ -0,0 +1,19 @@
+﻿// Домашнее задание
+// Семинар 6, Задача 44
+// Не используя рекурсию, вывести первые N чисел Фибоначчи
+
+Console.WriteLine("Введите количестов чисел Фибоначчи для вывода (число N)");
+int number = Convert.ToInt32(Console.ReadLine());
+int [] arrFibonacci = new int[number];
+
+for (int i = 0; i < number; i++)
+{
+    if (i < 2) arrFibonacci[i] = i;
+    else arrFibonacci[i] = arrFibonacci[i-1] + arrFibonacci[i-2];
+}
+
+Console.WriteLine($"Вывод последовательности чисел Фибоначчи ({number} первых членов).)");
+Console.WriteLine("["+ string.Join(", ", arrFibonacci)+ "]");
+
+
+

7f9f60839856f4924f94c40f4f96fae3d7294dbd
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-24 13:17:15 +0700

    HomeWork Seminar6 Task41

---
+++ Seminar6_Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Домашнее задание
+// Семинар 6, Задача 41
+// Ввод М чисел с клавиатуры, нахождение количества введённых чисел больше нуля
+
+Console.WriteLine("Сколько чисел будет введено?");
+int number = Convert.ToInt32(Console.ReadLine());
+int [] inputArray = new int[number];
+int positiveNumber = 0;
+
+for (int i = 0; i < number; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-е число");
+    inputArray[i] = Convert.ToInt32(Console.ReadLine());
+    if (inputArray[i] > 0) positiveNumber++;
+}
+
+Console.WriteLine("[" + string.Join(", ", inputArray) + "]");
+Console.WriteLine($"Введено чисел больше нуля: {positiveNumber}");
+

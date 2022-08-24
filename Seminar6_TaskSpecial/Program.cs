a73264ae6c5c98660fda33b196e7b2a94fcd0e63
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-08-24 21:08:46 +0700

    HomeWork Seminar6 Task Special

---
+++ Seminar6_TaskSpecial/Program.cs
@@ -0,0 +1,16 @@
+﻿// Домашнее задание
+// Семинар 6, Задача Дополнительная на рекурсию
+// Написать программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+Console.WriteLine("Введите число для расчёта факториала:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int MyFactorial(int numb)
+{
+    int result = 1;
+    if (numb == 1 || numb == 0) result = 1;
+    else result = numb * MyFactorial(numb - 1);
+    return result;
+}
+
+Console.WriteLine(MyFactorial(number));

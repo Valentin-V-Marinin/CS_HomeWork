de455844a0b9e538d89700e77b943d3a7d22c1c4
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-09-02 17:27:26 +0700

    HomeWork Seminar9 Tas64

---
+++ Seminar9_Task64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Домашнее задание
+// Семинар 8, Задача 64
+// Задать значение N. Написать программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void PrintN(int N)
+{
+    if (N == 0) return;
+    Console.Write(N + " ");
+    PrintN(N-1);
+}
+
+PrintN(N);
+Console.WriteLine();

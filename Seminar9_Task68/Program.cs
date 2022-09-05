9e44aa634e221ed966a698d5fe9147ce4990ce4d
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-09-05 11:15:32 +0700

    HomeWork Seminar9 Task68

--- Seminar9_Task68/Program.cs
+++ Seminar9_Task68/Program.cs
@@ -6,28 +6,22 @@ Console.WriteLine("Введите число m:");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число n:");
 int n = Convert.ToInt32(Console.ReadLine());
-bool endProgram = false;
 
 int Ackermann(int m, int n)
 {
-    while (!endProgram)
-    {
-        if (m == 0 && n > 0)
-            {
-                n++; endProgram = true;
-            } 
-        if (m > 0 && n == 0) 
-            {
-                Ackermann(m-1, 1);
-                if (m==1 && n==0) { (m,n)=(n,m);}
-            }
-        if (m > 0 && n > 0)  
-            {
-                Ackermann(m-1, Ackermann(m, n-1));
-                if (m==1 && n==1) {m=0; n=2;}
-            }
-    }
-    return (m + n);
+    if (m == 0 )
+        {
+            return ++n; 
+        }
+    if (m > 0 && n == 0) 
+        {
+            return Ackermann(m-1, 1); 
+        }
+    if   (m > 0 && n > 0)  
+        {
+            return Ackermann(m-1, Ackermann(m, n-1));
+        }
+    return n;
 }
 
-Console.WriteLine(Ackermann(m, n));
+
